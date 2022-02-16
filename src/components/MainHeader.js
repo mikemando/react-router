2176a37d4a2ee108eafb63f8dd182c544c2b5748
Michael Ikwuegbu <ikwuegbu300@gmail.com>
2022-02-16 09:51:16 -0800

    added code

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -1,16 +1,22 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+import classes from "./MainHeader.module.css";
+
 const MainHeader = () => {
     return (
-        <header>
+        <header className={classes.header}>
             <nav>
                 <ul>
                     <li>
-                        <NavLink to="/welcome">Welcome</NavLink>
+                        <NavLink activeClassName={classes.active} to="/welcome">
+                            Welcome
+                        </NavLink>
                     </li>
                     <li>
-                        <NavLink to="/product">Products</NavLink>
+                        <NavLink activeClassName={classes.active} to="/product">
+                            Products
+                        </NavLink>
                     </li>
                 </ul>
             </nav>
